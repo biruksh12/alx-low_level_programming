aae987f344362cd55e2b2f41aa268f9cdf6f2557
birukshi12 <birukshiferaw478@gmail.com>
2023-02-17 21:06:23 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual puzzle"
- * return: always 0 (sucess)
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return: always 0 (sucess)
  */
 int main(void)
 {
